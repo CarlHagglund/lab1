4db8d192b8247ddc5cb437372669849c34c2cfdc
Carl Hägglund <calle.hagglund@gmail.com>
2026-01-25 20:29:01 +0100

    volvo240 implementation fix

--- src/my/lab/cars/Volvo240.java
+++ src/my/lab/cars/Volvo240.java
@@ -4,24 +4,43 @@ import java.awt.*;
 
 public class Volvo240 extends Car implements Movable {
 
+    private boolean IsTurningLeft;
+    private boolean IsTurningRight;
+    private double CurrX;
+    private double CurrY;
+
     public Volvo240() {
         super(4, Color.black, 100, "Volvo240");
     }
 
     @Override
     public void move() {
+        if (IsTurningLeft) {
+            CurrX += super.getCurrentSpeed();
+        } else if (IsTurningRight) {
+            CurrY += super.getCurrentSpeed();
+        }
 
     }
 
     @Override
     public void turnLeft() {
-
+        IsTurningLeft = true;
+        IsTurningRight = false;
     }
 
     @Override
     public void turnRight() {
-
+        IsTurningRight = true;
+        IsTurningLeft = false;
     }
+
+    public double getCurrX() {
+        return CurrX;
+    }
+
+    public double getCurrY() {
+        return CurrY;
+    }
+
 }
-
-
