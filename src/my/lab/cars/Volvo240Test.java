14641e9e777bbb51c0a993e51c0acd5d5fa97eab
Carl Hägglund <calle.hagglund@gmail.com>
2026-01-27 12:58:40 +0100

    Added Volvo240Test mall

---
+++ src/my/lab/cars/Volvo240Test.java
@@ -0,0 +1,29 @@
+package my.lab.cars;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.assert;
+
+public class Volvo240Test {
+
+
+    @Test
+    public void testStartEngine() {
+
+        Volvo240 sut = new Volvo240();
+
+        sut.startEngine();
+
+        Assert.assertEquals();
+
+
+        @Test
+    public void testMove() {
+
+        Volvo240 sut = new Volvo240();
+        sut.move();
+
+        Assert.assertEquals();
+
+
+    }
+}
